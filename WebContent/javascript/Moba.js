3faf6d40d6f468c2cbd1aaa16026c47012995174
Meekgal <mpmadej@oakland.edu>
2016-08-28 15:17:28 -0400

    Standardizing BaseUnit. Added X and Y getters/setters. Also added a basic tower to test with.

--- WebContent/javascript/Moba.js
+++ WebContent/javascript/Moba.js
@@ -1,45 +1,68 @@
-var game = new Phaser.Game(800, 600, Phaser.CANVAS, 'Moba_Load_Area', { preload: preload, create: create, update: update, render: render });
+var game = new Phaser.Game(1000, 1000, Phaser.CANVAS, 'Moba_Load_Area', { preload: preload, create: create, update: update, render: render });
 
-function preload() {
+function preload() 
+{
 	game.load.image('hero', './images/CharacterTest.jpg');
+	game.load.image('tower', './images/towerTemp.png');
 	
 }
 
-var s;
+var tower;
+var hero;
+var tween;
 
-function create() {
-	
-    s = game.add.image(game.world.centerX, game.world.centerY, 'hero');
-    game.stage.backgroundColor = "#fff";
+function create() 
+{
+	//look into game.physics.arcade
+	hero = game.add.image(game.world.centerX, game.world.centerY, 'hero');
+	tower = game.add.image(game.world.center +20, game.world.centerY + 20, 'tower');
+    game.stage.backgroundColor = "#aaa";
     game.canvas.oncontextmenu = function (e) { e.preventDefault(); }
     
 }
 
-function update() {
+function testMouseClick(){
+	game.camera.follow();
+    game.physics.arcade.moveToPointer(hero, 100);
+}
 
-	if(game.input.mouse.button === Phaser.Mouse.RIGHT_BUTTON) {
-		game.add.tween(s).to({x:0,y:0},1000).start();    
-	    console.log("x:"+ game.input.mousePointer.x +"," + "y:" + game.input.mousePointer.y);
-	}
-	else{
-		console.log(game.input)
+function update() 
+{
+
+	var isStillDown;
+	if (game.input.activePointer.isDown && !isStillDown)
+    {
+		isStillDown= true;
+		var newX =game.input.mousePointer.x;
+		var newY =game.input.mousePointer.y;
+		var distance = Math.sqrt(Math.pow(newX - hero.x,2) + Math.pow(newY - hero.y,2));
+		var time = distance/ .5;
+		console.log("time:"+ time +"," + "distance:" + distance);
+		
+		tween =game.add.tween(hero).to({x:game.input.mousePointer.x,y:game.input.mousePointer.y},time,Phaser.Easing.Linear.None).start();    
+		
+		console.log("x:"+ game.input.mousePointer.x +"," + "y:" + game.input.mousePointer.y);
+    }
+	else if (game.input.activePointer.isUp)
+	{
+		isStillDown= false;
 	}
     if (game.input.keyboard.isDown(Phaser.Keyboard.LEFT))
     {
-        s.x -= 1;
+    	hero.x -= 1;
     }
     else if (game.input.keyboard.isDown(Phaser.Keyboard.RIGHT))
     {
-        s.x += 1;
+    	hero.x += 1;
     }
 
     if (game.input.keyboard.isDown(Phaser.Keyboard.UP))
     {
-        s.y -= 1;
+    	hero.y -= 1;
     }
     else if (game.input.keyboard.isDown(Phaser.Keyboard.DOWN))
     {
-        s.y += 1;
+    	hero.y += 1;
     }
 
 }
@@ -52,8 +75,11 @@ function render() {
 var Moba = Moba || {};
 
 	var BaseUnit = function(){
-		var HP;
-		var DEf;
+		var HP = 0;
+		var DEf = 0;
+		var Speed = 0;
+		var locationX;
+		var locationY;
 	}
 	BaseUnit.prototype.getHP = function(){
 		return this.HP;
@@ -65,3 +91,37 @@ var Moba = Moba || {};
 		//TODO: Add integer check
 		this.HP = HP;
 	}
+	BaseUnit.prototype.getSpeed = function(){
+		return this.DEF;
+	}
+	BaseUnit.prototype.setSpeed = function(Speed){
+		//TODO: Add integer check
+		this.Speed = Speed;
+	}
+	BaseUnit.prototype.setXLocation = function(locationX){
+		this.locationX = locationX;
+	}
+	BaseUnit.prototype.getXLocation = function(){
+		return locationX;
+	}
+	BaseUnit.prototype.setYLocation = function(locationY){
+		this.locationY = locationY;
+	}
+	BaseUnit.prototype.getYLocation = function(){
+		return locationY;
+	}
+
+	//            //
+	//Tower Object//
+	//            //
+	var tower = function(){	
+		var atkPower = 0;
+		tower.prototype = Object.Create(BaseUnit.prototype);
+	}
+	tower.prototype.attack =function()
+	{
+		
+		//generate tween that follows player.
+	}
+	
+	
